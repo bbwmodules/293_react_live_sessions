a6608aff4af46a7068347085b9bbed667611f161
Peter Rutschmann <bbwpr@iPhone.gibwintra.ch>
2025-11-13 13:24:07 +0100

    to publish, vite-config has to be fixed.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+// javascript
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mdx from '@mdx-js/rollup'
 
+const repo = process.env.GITHUB_REPOSITORY?.split('/')?.[1]
+const isGhActions = !!process.env.GITHUB_ACTIONS && !!repo
+
 export default defineConfig({
-  plugins: [react(), mdx()]
+    base: isGhActions ? `/${repo}/` : '/',
+    plugins: [react(), mdx()],
+    server: {
+        port: 5174
+    }
 })
